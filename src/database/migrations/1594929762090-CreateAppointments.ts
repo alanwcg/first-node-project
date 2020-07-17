05897a0afa48cb83cf37c827fccf1a1ac2c9bf5f
Alan Cintra <alancintra7@gmail.com>
2020-07-17 13:36:42 -0300

    Integrating App with DB

--- src/database/migrations/1594929762090-CreateAppointments.ts
+++ src/database/migrations/1594929762090-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1594929762090
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
