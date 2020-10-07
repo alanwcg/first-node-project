cffc577107e7b94a7fba414e774fb07dcc99008d
Alan Cintra <alancintra7@gmail.com>
2020-10-07 13:27:08 -0300

    Tests Fixes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -34,7 +34,7 @@ describe('CreateAppointment', () => {
       provider_id: '123456',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date,
         provider_id: '123456',
